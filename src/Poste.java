1f32cf4569f9f8862582428fb19ed25bd9d591ef
chakibx <chakib_moussaoui@outlook.fr>
2023-02-18 21:03:23 +0100

    bonjour

--- src/Poste.java
+++ src/Poste.java
@@ -3,5 +3,5 @@ package src;
 public class Poste {
     private Integer idPoste;
     private String libelle;
-    private Integer NombreHeuresParSemaine;
+    private Integer nombreHeuresParSemaine;
 }
--- src/SiteDeTravail.java
+++ src/SiteDeTravail.java
@@ -1,9 +1,7 @@
 package src;
 
-import java.util.ArrayList;
-
 public class SiteDeTravail {
     private Integer idSiteDeTravail;
     private String nomSiteDeTravail;
-    private String adresse;
+    private String adresseSiteDeTravail;
 }

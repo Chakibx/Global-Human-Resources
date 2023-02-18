c8944f76d7890f26d3429c0eee0caf57d103163b
chakibx <chakib_moussaoui@outlook.fr>
2023-02-18 21:18:50 +0100

    bonjour

--- src/Absence.java
+++ src/Absence.java
@@ -2,8 +2,8 @@ package src;
 import java.time.LocalDate;
 public class Absence {
     private Integer idAbsence;
-    private String motif;
+    private String motifAbsence;
     private Integer nombreDeJours;
-    private LocalDate date;
+    private LocalDate dateAbsence;
     private Employe employe;
 }

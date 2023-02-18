215a3829eb19add5d7931dd26caf4412d20626e9
chakibx <chakib_moussaoui@outlook.fr>
2023-02-18 21:20:28 +0100

    bonjour

--- src/FicheDePaie.java
+++ src/FicheDePaie.java
@@ -2,7 +2,7 @@ package src;
 import java.time.LocalDate;
 public class FicheDePaie {
     private Integer idFiche;
-    private LocalDate dateEcheance;
+    private LocalDate dateEcheanceFiche;
     private Float salaire;
     private Employe employe;
 }

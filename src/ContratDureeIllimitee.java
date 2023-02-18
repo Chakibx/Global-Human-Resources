58e79689c9d629f0034c9c130061408e8b80c807
chakibx <chakib_moussaoui@outlook.fr>
2023-02-18 21:06:44 +0100

    bonjour

--- src/ContratDureeIllimitee.java
+++ src/ContratDureeIllimitee.java
@@ -1,6 +1,6 @@
 package src;
 import java.time.LocalDate;
-public class ContratDureeIllimitee {
+public class ContratDureeIndeterminee {
     private Integer idContrat;
     private LocalDate debut;
     private Integer numeroContrat;

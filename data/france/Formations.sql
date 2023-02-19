821a4d063f96db1bc074b7fdf72328578cda7faa
chakibx <chakib_moussaoui@outlook.fr>
2023-02-19 11:56:11 +0100

    bonjour

--- data/france/Formations.sql
+++ data/france/Formations.sql
@@ -1,4 +1,4 @@
-INSERT INTO Formations (idFormation, typeFormation, coutFormation) VALUES
+INSERT INTO Formation (idFormation, typeFormation, coutFormation) VALUES
 (1, 'Formation en vente et négociation', 1000),
 (2, 'Formation en marketing digital', 1200),
 (3, 'Formation en communication interne', 1300),
@@ -8,6 +8,6 @@ INSERT INTO Formations (idFormation, typeFormation, coutFormation) VALUES
 (7, 'Formation en gestion financière', 800),
 (8, 'Formation en sécurité informatique', 700),
 (9, 'Formation en CISCO CCNA', 900),
-(10, 'Formation en gestion de projet', 500);
-(11,'Formation en Langue anglaise', 450),
+(10, 'Formation en gestion de projet', 500),
+(11,'Formation en Langue anglaise', 450);
 

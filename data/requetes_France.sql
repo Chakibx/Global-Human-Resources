a0110eeb8b7b352d1151d0efbf92a52a6936f588
chakibx <chakib_moussaoui@outlook.fr>
2023-02-20 20:15:45 +0100

    bonjour

--- data/requetes_France.sql
+++ data/requetes_France.sql
@@ -6,3 +6,9 @@ FROM departement D
          JOIN formation F ON F.id_formation = SF.id_formation
 WHERE EXTRACT(YEAR FROM SF.date_debut_formation) = EXTRACT(YEAR FROM CURRENT_DATE) - 1
 GROUP BY D.nom_departement;
+
+--Moyenne du salaire des employes ages entre 20 et 30 ans.
+SELECT AVG(c.salaire) AS moyenne_salaires
+FROM employe e
+         JOIN contrat_duree_indeterminee c ON e.id_employe = c.id_employe
+WHERE (CURRENT_DATE - e.date_de_naissance) BETWEEN 7300 AND 10950;

c228fa08d6e691816d2c2ce9959ac09492835716
AyoubCy <ayoubsalah0111@gmail.com>
2023-02-20 23:15:24 +0100

    mysql

--- data/france/Employe.sql
+++ data/france/Employe.sql
@@ -5,14 +5,14 @@ VALUES
 (3, 'Durand', 'Lucie', '06 09 10 11 12', '30 Rue du Pont, Marseille', '1987-03-01', 'bts', 3, 3),
 (4, 'Leblanc', 'Marie', '06 13 14 15 16', '40 Rue des Roses, Toulouse', '1992-04-01', 'bts', 4, 4),
 (5, 'Bernard', 'Paul', '06 17 18 19 20', '50 Rue de la Liberté, Nantes', '1988-05-01', 'bts', 1, 5),
-(6, 'Petit', 'Sophie', '06 21 22 23 24', '60 Rue du Moulin, Lille', '1995-06-01', 'licence', 2, 6),
+(6, 'Petit', 'Sophie', '06 21 22 23 24', '60 Rue du Moulin, Lille', '1995-06-01', 'licence', 2, 6),*
 (8, 'Moreau', 'Jacques', '06 29 30 31 32', '80 Rue des Vignes, Montpellier', '1991-08-01', 'licence', 4, 8),
 (9, 'Girard', 'Luc', '06 33 34 35 36', '90 Rue du Soleil, Nice', '1986-09-01', 'master', 1, 9),
 (10, 'Lefevre', 'Hélène', '06 37 38 39 40', '100 Rue du Lac, Strasbourg', '1989-10-01', 'master', 2, 10);
-(11, 'Lecomte', 'Julie', '06 41 42 43 44', '110 Rue des Fleurs, Rennes', '1993-11-01', 'master', 3, 11),
+(11, 'Lecomte', 'Julie', '06 41 42 43 44', '110 Rue des Fleurs, Rennes', '1993-11-01', 'master', 3, 11),*
 (12, 'Robert', 'David', '06 45 46 47 48', '120 Rue du Roi, Nancy', '1987-12-01', 'master', 4, 12),
 (13, 'Fournier', 'Caroline', '06 49 50 51 52', '130 Rue du Paradis, Lille', '1986-01-01', 'master', 1, 13),
-(14, 'Dumont', 'Sébastien', '06 53 54 55 56', '140 Rue des Moulins, Lyon', '1994-02-01', 'licence', 2, 14),
+(14, 'Dumont', 'Sébastien', '06 53 54 55 56', '140 Rue des Moulins, Lyon', '1994-02-01', 'licence', 2, 14),*
 (15, 'Mercier', 'Jérémy', '06 57 58 59 60', '150 Rue des Orangers, Nice', '1990-03-01', 'master', 3, 15),
 (16, 'Garcia', 'Anaïs', '06 61 62 63 64', '160 Rue des Chênes, Bordeaux', '1991-04-01', 'master', 4, 16),
 (17, 'Guillaume', 'Sophie', '06 65 66 67 68', '170 Rue des Boulevards, Toulouse', '1989-05-01', 'licence', 1, 17),
--- data/requetes_Usa.sql
+++ data/requetes_Usa.sql
@@ -1,9 +1,9 @@
---le cout total des formations de l'annee derniere
+--le cout total des formations de l'annee derniere(2)
 SELECT d.nomDepartement, SUM(f.coutFormation) AS total_cout_formation
 FROM Departement d
-INNER JOIN Employe e ON d.idDepartement = e.idDepartement
-INNER JOIN SeForme s ON e.idEmploye = s.idEmploye
-INNER JOIN Formation f ON s.idFormation = f.idFormation
+         INNER JOIN Employe e ON d.idDepartement = e.idDepartement
+         INNER JOIN SeForme s ON e.idEmploye = s.idEmploye
+         INNER JOIN Formation f ON s.idFormation = f.idFormation
 WHERE YEAR(s.dateDebutFormation) = YEAR(CURDATE()) - 1
 GROUP BY d.nomDepartement;
 
@@ -12,3 +12,57 @@ SELECT AVG(c.salaire) AS moyenne_salaires
 FROM Employe e
 JOIN ContratDureeIndeterminee c ON e.idEmploye = c.idEmploye
 WHERE DATEDIFF( CURDATE(),e.dateDeNaissance) BETWEEN 7300 AND 10950;
+
+--La liste des noms,fonctions, salaires des employes qui ont un salaire plus que 3500
+SELECT e.nom, p.libelle, cdi.salaire, cdi.dateDebutContrat
+FROM Employe e
+JOIN ContratDureeIndeterminee cdi ON e.idEmploye = cdi.idEmploye
+JOIN Poste p ON e.idPoste = p.idPoste
+WHERE cdi.salaire > 3500;
+
+
+
+--Le poste le plus performant de chaque departement avec le nom de l'emplyes et sa note (3)
+SELECT d.nomDepartement, p.libelle,e.nom, pf.notePerformance
+FROM Departement d
+JOIN Employe e ON e.idDepartement = d.idDepartement
+JOIN Performance pf ON pf.idEmploye = e.idEmploye
+JOIN Poste p ON e.idPoste = p.idPoste
+WHERE pf.notePerformance = (SELECT MAX(pf2.notePerformance)
+                            FROM Performance pf2
+                            JOIN Employe e2 ON e2.idEmploye = pf2.idEmploye
+                            WHERE e2.idDepartement = e.idDepartement)
+GROUP BY d.nomDepartement;
+
+
+--le nombre d'absence dans le departement ventes et marketing avec motif formation
+SELECT COUNT(*) as nb_absences
+FROM Absence a
+JOIN Employe e ON a.idEmploye = e.idEmploye
+JOIN Departement d ON e.idDepartement = d.idDepartement
+WHERE d.nomDepartement = 'Ventes et marketing' AND a.motifAbsence = 'Formation';
+
+--les employes avec leur poste et departement qui possede comme moyenne de performance <=12 pour effectuer des formations
+SELECT e.nom, p.libelle AS poste, d.nomDepartement AS departement ,pf.notePerformance
+FROM Employe e
+JOIN Performance pf ON e.idEmploye = pf.idEmploye
+JOIN Poste p ON e.idPoste = p.idPoste
+JOIN Departement d ON e.idDepartement = d.idDepartement
+GROUP BY e.nom, p.libelle, d.nomDepartement
+HAVING AVG(pf.notePerformance) <= 12;
+
+--la liste des postes libres classé par ordre croissant des salaires de bases
+SELECT p.libelle, p.salaireBase, p.nombreHeuresParSemaine
+FROM Poste p
+WHERE p.occupation = FALSE
+ORDER BY p.salaireBase ASC;
+
+--
+SELECT e.nom, pf1.notePerformance as note_avant, pf1.datePerformance AS date_note_avant, pf2.notePerformance as note_apres,pf2.datePerformance AS date_note_apres, f.typeFormation, sf.dateDebutFormation AS date_formation
+FROM SeForme sf
+JOIN Formation f ON sf.idFormation = f.idFormation
+JOIN Employe e ON sf.idEmploye = e.idEmploye
+JOIN Performance pf1 ON e.idEmploye = pf1.idEmploye AND pf1.datePerformance < sf.dateDebutFormation
+JOIN Performance pf2 ON e.idEmploye = pf2.idEmploye AND pf2.datePerformance > sf.dateDebutFormation
+WHERE pf2.notePerformance > pf1.notePerformance
+ORDER BY e.nom;

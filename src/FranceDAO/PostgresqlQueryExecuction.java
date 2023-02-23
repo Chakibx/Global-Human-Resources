6b571b3dfacd2b6d4e32c29d6d5d231eb2c44adf
chakibx <chakib_moussaoui@outlook.fr>
2023-02-23 23:51:37 +0100

    bonjour

---
+++ src/FranceDAO/PostgresqlQueryExecuction.java
@@ -0,0 +1,101 @@
+package src.FranceDAO;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class PostgresqlQueryExecuction {
+    private Connection connection;
+    private String query;
+    private Integer numeroQuery;
+
+    public void QueryExecution(Connection connection, String query, Integer numeroQuery) {
+        this.connection = connection;
+        this.query = query;
+        this.numeroQuery = numeroQuery;
+    }
+
+    public void Execute(){
+        try{
+            Statement statement = this.connection.createStatement();
+            ResultSet resultat = statement.executeQuery(query);
+            System.out.println("Colonnes: ");
+
+            switch (numeroQuery){
+                case 1:
+                    System.out.println("nomDepartement,total_cout_formation");
+                    System.out.println("******************************************************");
+                    while (resultat.next()) {
+                        System.out.println(resultat.getString("nom_departement")+","+resultat.getString("TotalCoutFormation"));
+                    }break;
+                case 2 :
+                    System.out.println("Moyenne,salaire");
+                    System.out.println("******************************************************");
+                    while (resultat.next()) {
+                        System.out.println(resultat.getString("moyenne_salaires"));
+                    }break;
+                case 3 :
+                    System.out.println("nom,libelle,salaire,dateDebutContrat");
+                    System.out.println("******************************************************");
+                    while (resultat.next()) {
+                        System.out.println(resultat.getString("nom")+","+resultat.getString("libelle")+","+resultat.getString("salaire")+","+resultat.getString("date_debut_contrat"));
+                    } break;
+                case 4:
+                    System.out.println("nomDepartement,libelle,nom,notePerformance");
+                    System.out.println("******************************************************");
+                    while (resultat.next()) {
+                        System.out.println(resultat.getString("nom_departement")+","+resultat.getString("libelle")+","+resultat.getString("nom")+","+resultat.getString("note_performance"));
+                    }
+                    break;
+                case 5:
+                    System.out.println("nb_absences");
+                    System.out.println("******************************************************");
+                    while (resultat.next()) {
+                        System.out.println(resultat.getString("nombre_absences"));
+                    }
+                    break;
+                case 6:
+                    System.out.println("nom,poste,departement,notePerformance");
+                    System.out.println("******************************************************");
+                    while (resultat.next()) {
+                        System.out.println(resultat.getString("nom")+","+resultat.getString("poste")+","+resultat.getString("departement")+","+resultat.getString("note_performance"));
+                    }
+                    break;
+                case 7:
+                    System.out.println("libelle,salaireBase,nombreHeuresParSemaine");
+                    System.out.println("******************************************************");
+                    while (resultat.next()) {
+                        System.out.println(resultat.getString("libelle")+","+resultat.getString("salaire_base")+","+resultat.getString("nombre_heures_par_semaine"));
+                    }
+                    break;
+                case 8:
+                    System.out.println("nom,note_avant,date_note_avant,note_apres,date_note_apres,typeFormation,date_formation,");
+                    System.out.println("******************************************************");
+                    while (resultat.next()) {
+                        System.out.println(resultat.getString("nom")+","+resultat.getString("note_avant")+","+resultat.getString("date_note_avant")+","+resultat.getString("note_apres")+","+resultat.getString("date_note_apres")+","+resultat.getString("type_formation")+","+resultat.getString("date_formation"));
+                    }
+                    break;
+                case 9:
+                    System.out.println("nom,libelle,augmentation,notePerformance");
+                    System.out.println("******************************************************");
+                    while (resultat.next()) {
+                        System.out.println(resultat.getString("nom")+","+resultat.getString("libelle")+","+resultat.getString("augmentation")+","+resultat.getString("note_performance"));
+                    }
+                    break;
+                case 10:
+                    System.out.println("nom,prenom,poste,departement,augmentation,performance");
+                    System.out.println("******************************************************");
+                    while (resultat.next()) {
+                        System.out.println(resultat.getString("nom")+","+resultat.getString("prenom")+","+resultat.getString("poste")+","+resultat.getString("departement")+","+resultat.getString("augmentation")+","+resultat.getString("performance"));
+                    }
+                    break;
+                default: return;
+            }
+
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 package src;
 import java.sql.*;
 import src.UsaDAO.MysqlConnection;
-import src.UsaDAO.Query;
+import src.UsaDAO.MysqlQuery;
 import src.UsaDAO.QueryExecution;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        Query query = new Query();
-        String sql = query.getQuery1();
-        QueryExecution execution = new QueryExecution(MysqlConnection.getConnection(),sql);
+        MysqlQuery query = new MysqlQuery();
+        String sql = query.getMSQL4();
+        QueryExecution execution = new QueryExecution(MysqlConnection.getConnection(),sql,4);
         execution.Execute();
     }
 }

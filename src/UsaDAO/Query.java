9910efb1baf67305ec9bf8bc7318969680a038aa
chakibx <chakib_moussaoui@outlook.fr>
2023-02-23 22:28:33 +0100

    bonjour

--- src/UsaDAO/Query.java
+++ src/UsaDAO/Query.java
@@ -2,9 +2,9 @@ package src.UsaDAO;
 
 
 public class Query {
-    private String query1 = "SELECT e.nom, p.libelle, cdi.salaire, cdi.dateDebutContrat FROM Employe e JOIN ContratDureeIndeterminee cdi ON e.idEmploye = cdi.idEmploye JOIN Poste p ON e.idPoste = p.idPoste WHERE cdi.salaire > 3500;";
-   /* private String query2 = "";
-    private String query3 = "";
+    private String 1 = "SELECT e.nom, p.libelle, cdi.salaire, cdi.dateDebutContrat FROM Employe e JOIN ContratDureeIndeterminee cdi ON e.idEmploye = cdi.idEmploye JOIN Poste p ON e.idPoste = p.idPoste WHERE cdi.salaire > 3500;";
+    private String query2 = "SELECT AVG(c.salaire) AS moyenne_salaires FROM Employe e JOIN ContratDureeIndeterminee c ON e.idEmploye = c.idEmploye WHERE DATEDIFF( CURDATE(),e.dateDeNaissance) BETWEEN 7300 AND 10950;";
+    /* private String query3 = "";
     private String query4 = "";
     private String query5 = "";
     private String query6 = "";
--- src/UsaDAO/QueryExecution.java
+++ src/UsaDAO/QueryExecution.java
@@ -5,22 +5,36 @@ import java.util.ArrayList;
 public class QueryExecution {
     private Connection connection;
     private String query;
-    ArrayList<String> data = new ArrayList<String>();
-    public QueryExecution(Connection connection, String query) {
+    private int numeroQuery;
+
+    public QueryExecution(Connection connection, String query, int numeroQuery) {
         this.connection = connection;
         this.query = query;
+        this.numeroQuery = numeroQuery;
     }
 
     public void Execute(){
         try{
+
             Statement statement = this.connection.createStatement();
             ResultSet resultat = statement.executeQuery(query);
             System.out.println("Colonnes: ");
-            System.out.println("Nom,Libelle,Salaire,Date début de contrat");
-            System.out.println("******************************************************");
-            while (resultat.next()) {
-                System.out.println(resultat.getString("nom")+","+resultat.getString("libelle")+","+resultat.getString("salaire")+","+resultat.getString("dateDebutContrat"));
-            };
+
+            switch (numeroQuery){
+                case 1 :
+                    System.out.println("nom,Libelle,Salaire,Date début de contrat");
+                    System.out.println("******************************************************");
+                    while (resultat.next()) {
+                        System.out.println(resultat.getString("nom")+","+resultat.getString("libelle")+","+resultat.getString("salaire")+","+resultat.getString("dateDebutContrat"));
+                    };
+                case 2 :
+                    System.out.println("nom,Libelle,Salaire,Date début de contrat");
+                    System.out.println("******************************************************");
+                    while (resultat.next()) {
+                        System.out.println(resultat.getString("nom")+","+resultat.getString("libelle")+","+resultat.getString("salaire")+","+resultat.getString("dateDebutContrat"));
+                    };
+            }
+
         }catch(Exception e){
             System.out.println(e.getMessage());
         }

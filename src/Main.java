78bda38bcd88d8ee8ca4c68b9ff47bdbeb5abb4e
chakibx <chakib_moussaoui@outlook.fr>
2023-02-23 23:53:46 +0100

    bonjour

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,13 @@ package src;
 import java.sql.*;
 import src.UsaDAO.MysqlConnection;
 import src.UsaDAO.MysqlQuery;
-import src.UsaDAO.QueryExecution;
+import src.UsaDAO.MysqlQueryExecution;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
         MysqlQuery query = new MysqlQuery();
         String sql = query.getMSQL4();
-        QueryExecution execution = new QueryExecution(MysqlConnection.getConnection(),sql,4);
+        MysqlQueryExecution execution = new MysqlQueryExecution(MysqlConnection.getConnection(),sql,4);
         execution.Execute();
     }
 }
--- src/UsaDAO/MysqlQueryExecution.java
+++ src/UsaDAO/MysqlQueryExecution.java
@@ -1,14 +1,12 @@
-package src.FranceDAO;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
+package src.UsaDAO;
+import java.sql.*;
 
-public class PostgresqlQueryExecuction {
+public class MysqlQueryExecution {
     private Connection connection;
     private String query;
     private Integer numeroQuery;
 
-    public void QueryExecution(Connection connection, String query, Integer numeroQuery) {
+    public MysqlQueryExecution(Connection connection, String query, Integer numeroQuery) {
         this.connection = connection;
         this.query = query;
         this.numeroQuery = numeroQuery;
@@ -16,72 +14,83 @@ public class PostgresqlQueryExecuction {
 
     public void Execute(){
         try{
+
             Statement statement = this.connection.createStatement();
             ResultSet resultat = statement.executeQuery(query);
-            System.out.println("Colonnes: ");
+
 
             switch (numeroQuery){
                 case 1:
+                    System.out.println("1 le cout total des formations de l'annee derniere(2)");
                     System.out.println("nomDepartement,total_cout_formation");
                     System.out.println("******************************************************");
                     while (resultat.next()) {
-                        System.out.println(resultat.getString("nom_departement")+","+resultat.getString("TotalCoutFormation"));
+                        System.out.println(resultat.getString("nomDepartement")+","+resultat.getString("total_cout_formation"));
                     }break;
                 case 2 :
-                    System.out.println("Moyenne,salaire");
+                    System.out.println("2 Moyenne du salaire des employes ages entre 20 et 30 ans.");
+                    System.out.println("Moyenne salaire");
                     System.out.println("******************************************************");
                     while (resultat.next()) {
                         System.out.println(resultat.getString("moyenne_salaires"));
                     }break;
                 case 3 :
+                    System.out.println("3 La liste des noms,fonctions, salaires des employes qui ont un salaire plus que 3500");
                     System.out.println("nom,libelle,salaire,dateDebutContrat");
                     System.out.println("******************************************************");
                     while (resultat.next()) {
-                        System.out.println(resultat.getString("nom")+","+resultat.getString("libelle")+","+resultat.getString("salaire")+","+resultat.getString("date_debut_contrat"));
+                        System.out.println(resultat.getString("nom")+","+resultat.getString("libelle")+","+resultat.getString("salaire")+","+resultat.getString("dateDebutContrat"));
                     } break;
                 case 4:
+                    System.out.println("4 Le poste le plus performant de chaque departement avec le nom de l'emplyes et sa note (3)");
                     System.out.println("nomDepartement,libelle,nom,notePerformance");
                     System.out.println("******************************************************");
                     while (resultat.next()) {
-                        System.out.println(resultat.getString("nom_departement")+","+resultat.getString("libelle")+","+resultat.getString("nom")+","+resultat.getString("note_performance"));
+                        System.out.println(resultat.getString("nomDepartement")+","+resultat.getString("libelle")+","+resultat.getString("nom")+","+resultat.getString("notePerformance"));
                     }
                     break;
                 case 5:
+                    System.out.println("5 le nombre d'absence dans le departement ventes et marketing avec motif formation");
                     System.out.println("nb_absences");
                     System.out.println("******************************************************");
                     while (resultat.next()) {
-                        System.out.println(resultat.getString("nombre_absences"));
+                        System.out.println(resultat.getString("nb_absences"));
                     }
                     break;
                 case 6:
+                    System.out.println("6 les employes avec leur poste et departement qui possede comme moyenne de performance <=12 pour effectuer des formations");
                     System.out.println("nom,poste,departement,notePerformance");
                     System.out.println("******************************************************");
                     while (resultat.next()) {
-                        System.out.println(resultat.getString("nom")+","+resultat.getString("poste")+","+resultat.getString("departement")+","+resultat.getString("note_performance"));
+                        System.out.println(resultat.getString("nom")+","+resultat.getString("poste")+","+resultat.getString("departement")+","+resultat.getString("notePerformance"));
                     }
                     break;
                 case 7:
+                    System.out.println("7 la liste des postes libres classé par ordre croissant des salaires de bases");
                     System.out.println("libelle,salaireBase,nombreHeuresParSemaine");
                     System.out.println("******************************************************");
                     while (resultat.next()) {
-                        System.out.println(resultat.getString("libelle")+","+resultat.getString("salaire_base")+","+resultat.getString("nombre_heures_par_semaine"));
+                        System.out.println(resultat.getString("libelle")+","+resultat.getString("salaireBase")+","+resultat.getString("nombreHeuresParSemaine"));
                     }
                     break;
                 case 8:
+                    System.out.println("8 l'impact des formations sur les performances");
                     System.out.println("nom,note_avant,date_note_avant,note_apres,date_note_apres,typeFormation,date_formation,");
                     System.out.println("******************************************************");
                     while (resultat.next()) {
-                        System.out.println(resultat.getString("nom")+","+resultat.getString("note_avant")+","+resultat.getString("date_note_avant")+","+resultat.getString("note_apres")+","+resultat.getString("date_note_apres")+","+resultat.getString("type_formation")+","+resultat.getString("date_formation"));
+                        System.out.println(resultat.getString("nom")+","+resultat.getString("note_avant")+","+resultat.getString("date_note_avant")+","+resultat.getString("note_apres")+","+resultat.getString("date_note_apres")+","+resultat.getString("typeFormation")+","+resultat.getString("date_formation"));
                     }
                     break;
                 case 9:
+                    System.out.println("9 le nom, la derniere performance, et le poste de l'employé qui a eu la plus grosse augmentation de salaire");
                     System.out.println("nom,libelle,augmentation,notePerformance");
                     System.out.println("******************************************************");
                     while (resultat.next()) {
-                        System.out.println(resultat.getString("nom")+","+resultat.getString("libelle")+","+resultat.getString("augmentation")+","+resultat.getString("note_performance"));
+                        System.out.println(resultat.getString("nom")+","+resultat.getString("libelle")+","+resultat.getString("augmentation")+","+resultat.getString("notePerformance"));
                     }
                     break;
                 case 10:
+                    System.out.println("10 tous les employes qui ont une aaugmentation de salaire <=700 et une performance <=15");
                     System.out.println("nom,prenom,poste,departement,augmentation,performance");
                     System.out.println("******************************************************");
                     while (resultat.next()) {
@@ -94,8 +103,6 @@ public class PostgresqlQueryExecuction {
         }catch(Exception e){
             System.out.println(e.getMessage());
         }
-    }
-
     }
 
 }

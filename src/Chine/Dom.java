5c534187098fb1418b4e964fba7d6a5fcce7229d
chakibx <chakib_moussaoui@outlook.fr>
2023-03-29 23:52:26 +0200

    bonjour

--- src/Chine/Dom.java
+++ src/Chine/Dom.java
@@ -1,16 +1,7 @@
 package src.Chine;
-
 import net.sf.saxon.s9api.*;
-import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
-
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.stream.StreamSource;
-import javax.xml.xpath.*;
 import java.io.File;
-import java.io.IOException;
 
 public class Dom {
     public static void xquery(Integer QueryNumber) throws SaxonApiException {
